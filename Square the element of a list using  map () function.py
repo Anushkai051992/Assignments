42b5818b7a218523c482880af5428350fc89957f
Anushka Singh <122049894+Anushkai051992@users.noreply.github.com>
2023-02-07 00:06:08 +0530

    Add files via upload

---
+++ Square the element of a list using  map () function.py
@@ -0,0 +1,11 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> 
+>>> def square_num(n):
+...   return n * n
+... nums = [4, 5, 2, 9]
+... print("Original List: ",nums)
+... result = map(square_num, nums)
+... print("Square the elements of the said list using map():")
